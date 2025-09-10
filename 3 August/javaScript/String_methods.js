0289a250f05265088e61c238bcb3dd4371da2f09
akshaymore3088 <morea1997@gmail.com>
2025-09-10 15:28:50 +0530

    StringLength

---
+++ 3 August/javaScript/String_methods.js
@@ -0,0 +1,11 @@
+const fullname = "Akshay";
+
+const stringLength = fullname.length;
+
+if (fullname.length > 0) {
+    console.log("Hello");
+} else {
+    console.log("bad");
+}
+
+console.log(stringLength);
